2ceee508c5c30216502a1f25bd6402b89157045f
Bernard065 <bebenibernard@gmail.com>
2023-03-19 08:08:05 +0300

    running migrations and seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Seeding..."
+
+# Create some restaurants
+restaurant1 = Restaurant.create(name: "Sottocasa NYC", address: "298 Atlantic Ave, Brooklyn, NY 11201")
+restaurant2 = Restaurant.create(name: "PizzArte", address: "69 W 55th St, New York, NY 10019")
+restaurant3 = Restaurant.create(name: "John's Pizza", address: "260 W 44th St, New York, NY 10036")
+restaurant4 = Restaurant.create(name: "L'asso Pizza", address: "192 Mott St, New York, NY 10012")
+
+# Create some pizzas
+pizza1 = Pizza.create(name: "Cheese", ingredients: "Dough, Tomato Sauce, Cheese")
+pizza2 = Pizza.create(name: "Pepperoni", ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni")
+pizza3 = Pizza.create(name: "Margherita", ingredients: "Dough, Tomato Sauce, Cheese, Basil")
+pizza4 = Pizza.create(name: "Hawaiian", ingredients: "Dough, Tomato Sauce, Cheese, Ham, Pineapple")
+
+# Create some restaurant pizzas
+restaurant_pizza1 = RestaurantPizza.create(price: 10, restaurant: restaurant1, pizza: pizza1)
+restaurant_pizza2 = RestaurantPizza.create(price: 12, restaurant: restaurant1, pizza: pizza2)
+restaurant_pizza3 = RestaurantPizza.create(price: 8, restaurant: restaurant2, pizza: pizza3)
+restaurant_pizza4 = RestaurantPizza.create(price: 15, restaurant: restaurant3, pizza: pizza4)
+restaurant_pizza5 = RestaurantPizza.create(price: 20, restaurant: restaurant4, pizza: pizza2)
+restaurant_pizza6 = RestaurantPizza.create(price: 25, restaurant: restaurant4, pizza: pizza3)
+restaurant_pizza7 = RestaurantPizza.create(price: 30, restaurant: restaurant4, pizza: pizza4)
+restaurant_pizza8 = RestaurantPizza.create(price: 5, restaurant: restaurant2, pizza: pizza1)
+
+
+puts "Done seeding"
