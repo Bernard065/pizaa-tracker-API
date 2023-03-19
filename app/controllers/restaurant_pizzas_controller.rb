114c3960fca6c40d4c8273134def9696bfce1538
Bernard065 <bebenibernard@gmail.com>
2023-03-19 11:22:46 +0300

    correcting the errors associated with post restaurant pizzas

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,24 +1,29 @@
 class RestaurantPizzasController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-
+  
     def create
-        new_restaurant_pizza = RestaurantPizza.create!(restaurant_pizza_params)
-        render json: new_restaurant_pizza, status: :created
+      restaurant_pizza = RestaurantPizza.new(restaurant_pizza_params)
+  
+      if restaurant_pizza.save
+        render json: restaurant_pizza.pizza, status: :created
+      else
+        render json: { errors: restaurant_pizza.errors.full_messages }, status: :unprocessable_entity
+      end
     end
-
+  
     private
-
+  
     def restaurant_pizza_params
-        params.permit(:price, :pizza_id, :restaurant_id)
+      params.permit(:price, :pizza_id, :restaurant_id)
     end
-
+  
     def render_not_found_response
-        render json: { error: "Restaurant pizza not found" }
+      render json: { error: "Restaurant pizza not found" }, status: :not_found
     end
-
+  
     def render_unprocessable_entity_response(invalid)
-        render json: { errors: invalid.record.error.full_message }, status: :unprocessable_entity
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
-
-end
+  end
+  
